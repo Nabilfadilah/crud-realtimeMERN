cf9f159fcddaec12cd4e93e43b5a62a6bded5619
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-05-13 14:04:52 +0700

    import router ke index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import ProductRoute from "./routes/ProductRoute.js"
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express()
 // midleware
 app.use(cors());
 app.use(express.json());
+app.use(ProductRoute);
 
 app.listen(process.env.APP_PORT, () => {
     console.log('server up and running...')
